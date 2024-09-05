7d004dddd37783c608ab0d3f73658a8906361573
Thomaz Szeckir <thomazszeckir@Thomazs-Air.lan>
2024-09-05 12:39:03 -0400

    :construction: adicionado icones, telas login e signup

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '../components/ScreenWrapper'
 import { StatusBar } from 'expo-status-bar'
 import { hp, wp } from '../helpers/common'
 import { theme } from '../constants/theme'
 import Button from '../components/Button'
+import { useRouter } from 'expo-router'
 
 const Welcome = () => {
+    const router = useRouter();
   return (
     <ScreenWrapper bg='white'>
         <StatusBar style='dark'/>
@@ -15,18 +17,31 @@ const Welcome = () => {
             <Image style={styles.welcomeImage} resizeMode='contain' source={require('../assets/images/welcome.png')} />
 
             {/* title */}
-            <View style={{gap: 20}}>
-                <Text style={styles.title}>LinkUp!</Text>
+            <View style={{gap:20}}>
+                <Text style={styles.title}>DevApp!</Text>
                 <Text style={styles.punchLine}>
-                    Where every tought finds a home and every image tells a story.
+                   Onde todos os pensamentos viram realidade e a criatividade é o limite.
                 </Text>
             </View> 
 
             {/* */ }
             <View style={styles.footer}>
-                <Button styles={{
-                    backgroundColor: 'red'
-                }}/>
+                <Button 
+                    title="Comece agora"
+                    buttonStyle={{marginHorizontal: wp(3)}}
+                    onPress={() => router.push('signUp')}
+                />
+                <View style={styles.bottomTextContainer}>
+                    <Text style={styles.loginText}>
+                        Você já tem uma conta!
+                    </Text>
+                    <Pressable onPress={() => router.push('login')}>
+                        <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold}]}>
+                            Login
+                        </Text>
+                    </Pressable>
+
+                </View>
             </View>
         </View>
     </ScreenWrapper>
@@ -40,6 +55,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'space-between',
+        marginBottom: hp(5),
         backgroundColor: 'white',
         paddingHorizontal: wp(4),
     },
@@ -59,5 +75,20 @@ const styles = StyleSheet.create({
         paddingHorizontal: wp(10),
         fontSize: hp(1.7),
         color: theme.colors.text,
+    }, 
+    footer: {
+        gap: 30,
+        width: '100%',
+    },
+    bottomTextContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: 5,
+    },
+    loginText: {
+        textAlign: 'center',
+        color: theme.colors.text,
+        fontSize: hp(1.6),
     }
 })
--- components/Button.jsx
+++ components/Button.jsx
@@ -1,19 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { theme } from '../constants/theme'
+import { hp } from '../helpers/common'
+import Loading from './Loading'
 
-const Button = () => {
   const Button = ({
     buttonStyle,
     textStyle,
-  });
+    title='',
+    onPress=() =>{},
+    loading = false,
+    hasShadow = true,
 
+  }) => {
+    const shadowStyle = {
+        shadowColor: theme.colors.dark,
+        shadowOffset: {width: 0, height: 10},
+        shadowOpacity: 0.2,
+        shadowRadius: 8,
+        elevation: 4
+    }
+
+    if(loading){
+      return (
+        <View style={[styles.button, buttonStyle, {backgroundColor: 'white'}]}>
+          <Loading />
+        </View>
+    )
+  }
   return (
-    <View>
-      <Text>Button</Text>
-    </View>
+    <Pressable onPress={onPress} style={[styles.button, buttonStyle, hasShadow && shadowStyle]}>
+      <Text style={[styles.text, styles.textStyle]}>{title}</Text>
+    </Pressable>
   )
 }
 
 export default Button
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  button:{
+    backgroundColor: theme.colors.primary,
+    height: hp(6.6),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderCurve: 'continuos',
+    borderRadius: theme.radius.xl
+  },
+  textStyle: {
+    fontSize: hp(2.5),
+    color: 'white',
+    fontWeight: theme.fonts.bold
+  }
+});
