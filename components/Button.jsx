f7ee9ce5f6eb0044304d70e9d8fa22b0917074b4
Thomaz Szeckir <thomazszeckir@Thomazs-Air.lan>
2024-09-04 22:00:47 -0400

    first commit

---
+++ components/Button.jsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Button = () => {
+  const Button = ({
+    buttonStyle,
+    textStyle,
+  });
+
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  )
+}
+
+export default Button
+
+const styles = StyleSheet.create({})
